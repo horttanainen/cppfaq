be443f0f8205d013082adf3c71982ca212ec4b16
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-04-06 21:25:36 +0300

    Started q2 but could not finish.

---
+++ q2.cpp
@@ -0,0 +1,60 @@
+#include <iterator>
+#include <iostream>
+#include <forward_list>
+
+namespace cppfaq
+{
+	// I learned that I do not know much about iterators and especially how restricted forwardIterators truly are. 
+	// This was my first attempt:
+	//
+	//template<class ForwardIterator>
+	//void reverseImpl(ForwardIterator first, ForwardIterator last, std::forward_iterator_tag)
+	//{
+	//	using T = std::iterator_traits<ForwardIterator>::value_type;
+	//	auto length = last - first;
+	//	T storedValues[length];
+	//	for (auto i = 0; i < length; ++i)
+	//	{
+	//		storedValues[i] = *(first + i);
+	//	}
+	//	for (auto i = length - 1; first != last; --i)
+	//	{
+	//		*(first++) = storedValues[i];
+	//	}
+	//}
+
+	template<class ForwardIterator>
+	void reverseImpl(ForwardIterator first, ForwardIterator last, std::forward_iterator_tag)
+	{
+		auto middle = first;
+		std::advance(middle, std::distance(first, last) / 2);
+
+		std::rotate(first, middle, last);
+	}
+
+	template<class ForwardIterator>
+	void reverse(ForwardIterator first, ForwardIterator last)
+	{
+		using category = std::iterator_traits<ForwardIterator>::iterator_category;
+		reverseImpl(first, last, category());
+	}
+}
+
+int main()
+{
+	std::forward_list<int> l = {1,2,3,4,5,6,7,8,9};
+
+	for (const auto& el : l)
+	{
+		std::cout << el << ", ";
+	}
+	std::cout << std::endl;
+
+	cppfaq::reverse(l.begin(), l.end());
+
+	for (const auto& el : l)
+	{
+		std::cout << el << ", ";
+	}
+	std::cout << std::endl;
+}
